bd13b29bd9fd030a4bb2e276135bb612d4555c7b
philip <philip@384ed961-0df3-0310-a715-f7914cc3aac3>
2005-04-11 03:35:10 +0000

    fixed relative dir

--- LocalWiki/macro/Stats.py
+++ LocalWiki/macro/Stats.py
@@ -24,14 +24,14 @@ def execute(macro, args):
        htmltext = []
        for user in users:
           if user.getAttribute("name") == args:
-	     htmltext.append('<p><h2>%s\'s Statistics</h2></p><table width=100%% border=0><tr><td><b>Pages Edited&nbsp;&nbsp;</b></td><td><b>Pages Created&nbsp;&nbsp;</b></td><td><b>Images Contributed&nbsp;&nbsp;</b></td><td><b>Date Joined&nbsp;&nbsp;</b></td><td><b>Last Edit&nbsp;&nbsp;</b></td><td><b>Last Page Edited&nbsp;&nbsp;</b></td></tr>' % args)
+	     htmltext.append('<p><h2>%s\'s Statistics</h2></p><table width=100%% border=0><tr><td><b>Edits&nbsp;&nbsp;</b></td><td><b>Pages Created&nbsp;&nbsp;</b></td><td><b>Images Contributed&nbsp;&nbsp;</b></td><td><b>Date Joined&nbsp;&nbsp;</b></td><td><b>Last Edit&nbsp;&nbsp;</b></td><td><b>Last Page Edited&nbsp;&nbsp;</b></td></tr>' % args)
              htmltext.append('<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td><a href="/%s%s%s">%s</a></td></tr></table>' % (user.getAttribute("edit_count"),user.getAttribute("created_count"),user.getAttribute("file_count"),user.getAttribute("join_date"),user.getAttribute("last_edit"),config.relative_dir,add_on,wikiutil.quoteWikiname(user.getAttribute("last_page_edited")),user.getAttribute("last_page_edited")))	     
     else:
        htmltext = []
        list = []
        dom = xml.dom.minidom.parse(config.app_dir + "/userstats.xml")
        users = dom.getElementsByTagName("user")
-       htmltext.append('<p><h2>User Statistics</h2></p><table width=100%% border=0><tr><td><b>User</b></td><td><b>Pages Edited&nbsp;&nbsp;</b></td><td><b>Pages Created&nbsp;&nbsp;</b></td><td><b>Images Contributed&nbsp;&nbsp;</b></td><td><b>Date Joined&nbsp;&nbsp;</b></td><td><b>Last Edit&nbsp;&nbsp;</b></td><td><b>Last Page Edited&nbsp;&nbsp;</b></td></tr>')
+       htmltext.append('<p><h2>User Statistics</h2></p><table width=100%% border=0><tr><td><b>User</b></td><td><b>Edits&nbsp;&nbsp;</b></td><td><b>Pages Created&nbsp;&nbsp;</b></td><td><b>Images Contributed&nbsp;&nbsp;</b></td><td><b>Date Joined&nbsp;&nbsp;</b></td><td><b>Last Edit&nbsp;&nbsp;</b></td><td><b>Last Page Edited&nbsp;&nbsp;</b></td></tr>')
        for user in users:
           list.append(user) 
        list.sort(compare_edit)
