2bb7a4b4521dd2424fde73606d34b5acee8675f8
brentlaabs <brentlaabs@384ed961-0df3-0310-a715-f7914cc3aac3>
2005-12-07 02:48:07 +0000

    updates dethumb.pl to use fiximagename.py

--- sycamore/util_apps/DBConversion/dethumb.pl
+++ sycamore/util_apps/DBConversion/dethumb.pl
@@ -22,19 +22,27 @@ close WIKIPAGE;
 $i = 0;
 
 while ($wiki =~ /\{\{\{.*?\}\}\}/s) {
-        if ( $wiki =~ s/(\{\{\{.*?\}\}\})/smurf$i/s) {
-                $comments[$i] = $1;
-        }
-        $i++;
+	if ( $wiki =~ s/(\{\{\{.*?\}\}\})/smurf$i/s) {
+		$comments[$i] = $1;
+	}
+	$i++;
 }
 
 
 while ($wiki =~ /attachment:/s  ) {
-        $wiki =~ s/attachment:(\w+?\.\w\w\w\w?)(\W)/[[Image($1)]]$2/s;
+	if ($wiki =~ s/attachment:(.*?)([\s|])/smurfette0/s) {
+		$foo = $1;  $bar = $2;  }
+	$foo = `python fiximagename.py $foo`;
+	chop($foo);
+	$wiki =~ s/smurfette0/[[Image($foo)]]$bar/s;
 }
 
 while ($wiki =~ /borderless:/s  ) {
-        $wiki =~ s/borderless:(\w+?\.\w\w\w\w?)(\W)/[[Image($1, noborder)]]$2/s;
+	if ($wiki =~ s/borderless:(.*?)([\s|])/smurfette0/s) {
+		$foo = $1;  $bar = $2;  }
+	$foo = `python fiximagename.py $foo`;
+	chop($foo);
+	$wiki =~ s/smurfette0/[[Image($foo, noborder)]]$bar/s;
 }
 
 
@@ -42,42 +50,44 @@ while ($wiki =~ /borderless:/s  ) {
 
 # thumbnails!
 while($wiki =~ /\[\[Thumbnail/    ) {
-        if ($wiki =~ s/\[\[Thumbnail\((.+?)\)\]\]/smurfsmurf/ ) {
-                $thumbargs = $1;
-        }
-        $caption = undef;
-        # get caption, if exists
-        if ($thumbargs =~ /".*"/) {
-                if ($thumbargs =~ s/,\s*"(.*)"//) {
-                        $caption = $1;
-                }
-        }
-        #clear whitespace
-        $thumbargs =~ s/\s//g;
-        @thumbarg = undef;
-        # get remaining arguments
-        @thumbarg = split /,/, $thumbargs;
-        $imagefile = $thumbarg[0];
-        $imagesize = $leftitude = undef;
-        for($k=1; $k <= $#thumbarg; $k++) {
-                $thumbarg[$k] =~ /\d/ and $imagesize = $thumbarg[$k];
-                $thumbarg[$k] =~ /left/ and $leftitude = $thumbarg[$k];
-                $thumbarg[$k] =~ /right/ and $leftitude = $thumbarg[$k];
-        }
-        $imagestring = '[[Image(' . $imagefile;
-        defined($caption) and   $imagestring = $imagestring . ', "' . $caption . '"';
-        defined($imagesize) and $imagestring = $imagestring . ', ' . $imagesize;
-        defined($leftitude) and  $imagestring = $imagestring . ', ' . $leftitude;
-        
-        $imagestring = $imagestring . ', ' . 'thumbnail)]]';
- #      print $imagestring; 
-        # insert new image tag
-        $wiki =~ s/smurfsmurf/$imagestring/;
+	if ($wiki =~ s/\[\[Thumbnail\((.+?)\)\]\]/smurfsmurf/ ) {
+		$thumbargs = $1;
+	}
+	$caption = undef;
+	# get caption, if exists
+	if ($thumbargs =~ /".*"/) {
+		if ($thumbargs =~ s/,\s*"(.*)"//) {
+			$caption = $1;
+		}
+	}
+	#clear whitespace
+	$thumbargs =~ s/\s//g;
+	@thumbarg = undef;
+	# get remaining arguments
+	@thumbarg = split /,/, $thumbargs;
+	$imagefile = `python fiximagename.py $thumbarg[0]`;
+	chop($imagefile);
+	$imagesize = $leftitude = undef;
+	for($k=1; $k <= $#thumbarg; $k++) {
+		$thumbarg[$k] =~ /\d/ and $imagesize = $thumbarg[$k];
+		$thumbarg[$k] =~ /left/ and $leftitude = $thumbarg[$k];
+		$thumbarg[$k] =~ /right/ and $leftitude = $thumbarg[$k];
+	}
+	$imagestring = '[[Image(' . $imagefile;
+	defined($caption) and   $imagestring = $imagestring . ', "' . $caption . '"';
+	defined($imagesize) and $imagestring = $imagestring . ', ' . $imagesize;
+	defined($leftitude) and  $imagestring = $imagestring . ', ' . $leftitude;
+	
+	$imagestring = $imagestring . ', ' . 'thumbnail)]]';
+ #	print $imagestring; 
+	# insert new image tag
+	$wiki =~ s/smurfsmurf/$imagestring/;
 }
 
 
+#replace comments
 while ($wiki =~ /smurf\d/) {
-        $wiki =~ s/smurf(\d+)/$comments[$1]/s;
+	$wiki =~ s/smurf(\d+)/$comments[$1]/s;
 }
 
 
@@ -87,3 +97,5 @@ print WIKIOUT $wiki;
 close WIKIOUT;
 
 print "exit success\n";
+
+
