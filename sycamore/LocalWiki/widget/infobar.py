0a76232fcc7c121def0d2eeb393a361e21079cbb
dreid <dreid@384ed961-0df3-0310-a715-f7914cc3aac3>
2005-12-10 00:40:17 +0000

    add infobar widget

---
+++ sycamore/LocalWiki/widget/infobar.py
@@ -0,0 +1,50 @@
+from LocalWiki.widget import base
+from LocalWiki.user import getUserId
+from LocalWiki import wikiutil
+
+def isSubscribed(request, pagename):
+    return request.user.isFavoritedTo(pagename) or not request.user.valid
+
+def isUserPage(request, pagename):
+    return getUserId(pagename)
+
+class InfoBar(base.Widget):
+    #Display, query args, should this be displayed
+    infoTabs = [['Revision History', 'action=info', None],
+		['General Info', 'action=info&general=1', None],
+		['Images', 'action=Files', None],
+		["User's Edits", 'action=useredits', isUserPage],
+		['Add to wiki bookmarks', 'action=favorite', isSubscribed]]
+
+    before, after = '[', ']'
+    seperator = ' '
+
+    def __init__(self, request, pagename):
+	self.request = request
+	self.pagename = pagename
+
+    def render_link(self, tab):
+	_ = self.request.getText
+
+	if self.request.query_string == tab[1]:
+	    link = _(tab[0])
+	else:
+	    link = wikiutil.link_tag(self.request, 
+				     '%s?%s' % (wikiutil.quoteWikiname(self.pagename), 
+					       tab[1]), _(tab[0]))
+
+	self.request.write(self.before + link + self.after)
+
+    def render(self):
+	self.request.write('<p>')
+
+	for tab in self.infoTabs:
+	    if callable(tab[2]):
+		if not tab[2](self.request, self.pagename):
+		    continue
+	    
+	    self.render_link(tab)
+	    self.request.write(self.seperator)
+	
+	self.request.write('</p>')
+	
