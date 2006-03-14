d9feba0e2bdc16fa71f4fbdc875de85436d8f85c
philip <philip@384ed961-0df3-0310-a715-f7914cc3aac3>
2006-03-14 21:52:03 +0000

    Info bar tabs.

--- Sycamore/widget/infobar.py
+++ Sycamore/widget/infobar.py
@@ -16,8 +16,8 @@ class InfoBar(base.Widget):
 		["User's Info", 'action=userinfo', isUserPage],
 		['Add to wiki bookmarks', 'action=favorite', isNotSubscribed]]
 
-    before, after = '<span>', '</span>'
-    before_active, after_active = '<span class="actionBoxSelected">', '</span>'
+    before, after = '<li class="lightTabs">', '</li>'
+    before_active, after_active = '<li class="lightTabsActive">', '</li>'
     seperator = ' '
 
     def __init__(self, request, pagename):
@@ -39,7 +39,7 @@ class InfoBar(base.Widget):
 	    self.request.write('%s%s%s' % (self.before, link, self.after))
 
     def render(self):
-	self.request.write('<div class="actionBoxes">')
+	self.request.write('<div class="lightTabsContainer"><div class="lightTabsStart">&nbsp;</div><ul class="lightTabs">')
 
 	for tab in self.infoTabs:
 	    if callable(tab[2]):
@@ -49,4 +49,4 @@ class InfoBar(base.Widget):
 	    self.render_link(tab)
 	    self.request.write(self.seperator)
 	
-	self.request.write('</div><div style="clear: both;"></div>')
+	self.request.write('</ul></div><div class="lightTabsFin"></div>')
