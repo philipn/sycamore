f5638451133765104bfa909e0c612165ee11df8c
philip <philip@384ed961-0df3-0310-a715-f7914cc3aac3>
2006-04-12 05:33:34 +0000

    Minor: using config.relative_dir in getScriptname

--- Sycamore/request.py
+++ Sycamore/request.py
@@ -780,10 +780,11 @@ class RequestWSGI(RequestBase):
 
     def getScriptname(self):
         """ Return the scriptname part of the URL ('/path/to/my.cgi'). """
-        name = self.script_name
-        if name == '/':
-            return ''
-        return name
+        return config.relative_dir
+        #name = self.script_name
+        #if name == '/':
+        #    return ''
+        #return name
 
 
     def getPathinfo(self):
