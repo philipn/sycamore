03ae2310c10def77160be31d4114b827d0484fab
philip <philip@384ed961-0df3-0310-a715-f7914cc3aac3>
2006-05-30 10:21:06 +0000

    RequestDummy objects need to call db_disconnect() when they're done if they want to commit :)e

--- Sycamore/rebuildPageCaches.py
+++ Sycamore/rebuildPageCaches.py
@@ -1,27 +1,35 @@
 import sys, cStringIO
-sys.path.extend(['/srv/wikis/daviswiki/trunk'])
+sys.path.extend(['/srv/wikis/rocwiki/trunk'])
 import __init__
 from Sycamore import wikiutil, config, request, caching, wikidb
 from Sycamore.Page import Page
 
-def clearCaches():
+def clearCaches(req):
   print "Clearing page caches..."
-  req = request.RequestDummy()
   plist = wikiutil.getPageList(req)
   for pname in plist:
     key = pname
+    print key
     cache = caching.CacheEntry(key, req)
     cache.clear()
+  print "XXXXXXXXXXXXXXXXXXXXXXXXXXX"
+  print "cleared page caches!"
+  print "XXXXXXXXXXXXXXXXXXXXXXXXXXX"
 
-def buildCaches():
+def buildCaches(req):
   print "Building page caches...It is _normal_ for this to produce errors!"
   # this is hackish, but it will work
   # the idea is to view every page to build the cache
   # we should actually refactor send_page()
-  req = request.RequestDummy()
   for pname in wikiutil.getPageList(req):
    print pname
    Page(pname, req).getPageLinks(docache=True)
+  print "XXXXXXXXXXXXXXXXXXXXXXXXXXX"
+  print "rebuilt page caches!"
+  print "XXXXXXXXXXXXXXXXXXXXXXXXXXX"
 
-clearCaches()
-buildCaches()
+
+req = request.RequestDummy()
+clearCaches(req)
+buildCaches(req)
+req.db_disconnect()
