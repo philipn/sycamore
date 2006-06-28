c46a033beb9b20344cdd48c46f60d438a002bdb8
philip <philip@384ed961-0df3-0310-a715-f7914cc3aac3>
2006-06-28 14:52:01 +0000

    datetime=None issue

--- Sycamore/Page.py
+++ Sycamore/Page.py
@@ -243,7 +243,9 @@ class Page(object):
         @return: mtime of page (or 0 if page does not exist)
         """
 	if not self.prev_date:
-	   if self.last_edit_info(): return self.last_edit_info()[0]
+	   info = self.last_edit_info()
+	   if info and info[0]:
+	     return info[0]
 	   return 0
         else:
 	  self.cursor.execute("SELECT editTime from allPages where name=%(page_name)s and editTime <= %(prev_date)s order by editTime desc limit 1;", {'page_name':self.page_name, 'prev_date':self.prev_date})
