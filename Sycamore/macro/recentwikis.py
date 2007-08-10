bb016e96657621c7db60edcf8d62b3051efe495a
cmclaughlin@ucdavis.edu <cmclaughlin@ucdavis.edu@384ed961-0df3-0310-a715-f7914cc3aac3>
2007-08-10 06:45:21 +0000

    Fixed mysql related bug, added prev/next links

--- Sycamore/macro/recentwikis.py
+++ Sycamore/macro/recentwikis.py
@@ -20,20 +20,58 @@ def execute(macro, args, formatter=None):
             return recent_wikis
 
     text = []
+
+
+    # only show 100 wikis, determine where to start
+
+    start = 0
+
+    if request.form.has_key('next'):
+      try:
+        start = int(request.form['next'][0]) + 100
+      except(ValueError, TypeError):
+        return('')    # evil user manipulated the url
+
+    if request.form.has_key('prev'):
+      try:
+        start = int(request.form['prev'][0]) - 100
+      except(ValueError, TypeError):
+        return('')    # evil user manipulated the url
+            
+      if start < 0:
+        start = 0     # mysql wouldn't like LIMIT -100, 100
+
+    if start > 0:
+      text.append('<a href=?prev=%s>Previous Page</a><br>' % start)
+
+    request.cursor.execute("""SELECT COUNT(name) as wikiCount FROM wikis""")
+    totalNumRows = request.cursor.fetchone()
+
+
+    if start + 100 < totalNumRows[0]:
+      text.append('<a href=?next=%s>Next page</a>' % start)
+    else:
+      start = totalNumRows[0] - 100   # user messed with the url
+                                      # show the last page
+                                    
     
     text.append(formatter.bullet_list(1))
-    request.cursor.execute("""SELECT wikis.name, min(editTime)
+    request.cursor.execute("""SELECT wikis.name, min(editTime) as min
                               FROM allPages, wikis
                               WHERE editTime > 0 and
                                     allPages.wiki_id=wikis.id
                               GROUP BY wikis.name
                               ORDER BY min
-                              DESC LIMIT 100""")
-    for item in request.cursor.fetchall():
+                              DESC LIMIT %s, 100""" % start)
+
+    limitedRows = request.cursor.fetchall()
+
+    for item in limitedRows:
         wiki_name, editTime = item
         link = farm.link_to_wiki(wiki_name, formatter)
         text.append('%s%s%s' %
                     (formatter.listitem(1), link, formatter.listitem(0)))
+        
     text.append(formatter.bullet_list(0))
 
     recent_wikis = formatter.rawHTML(''.join(text))
