e15d43b57054c1915c19bb85f19b3b4776ae5f72
philipn@gmail.com <philipn@gmail.com@384ed961-0df3-0310-a715-f7914cc3aac3>
2009-03-10 03:35:06 +0000

    making wiki creation process more straightforward

--- Sycamore/macro/makewiki.py
+++ Sycamore/macro/makewiki.py
@@ -9,6 +9,9 @@ Dependencies = ['time']
 
 def execute(macro, args, formatter=None):
     request = macro.request
+    before_form, after_form = '', ''
+    if args and len(args.split(',')) == 2:
+        before_form, after_form = args.split(',')
     if not formatter:
         formatter = macro.formatter
     if not config.allow_web_based_wiki_creation:
@@ -17,15 +20,24 @@ def execute(macro, args, formatter=None):
     if request.user.valid:
             d = {'form_action': formatter.page.url(),
                 'wikiname_length': farm.WIKINAME_MAX_LENGTH,
-                'create_wiki_button': 'send_email'}
+                'create_wiki_button': 'send_email',
+                'after_form': after_form,
+                'before_form': before_form,
+            }
             wiki_create_form = (
                 '<form action="%(form_action)s" method="POST">\n'
+                '<span class="wikiNameArea">'
                 '<input type="hidden" name="action" value="new_wiki">\n'
-                '<input type="text" name="wikiname" size="20" '
-                       'maxlength="%(wikiname_length)s">\n'
+                '%(before_form)s'
+                '<input type="text" class="wikiname" name="wikiname" '
+                       'size="15" value="shortname" '
+                       'maxlength="%(wikiname_length)s" '
+                       'onFocus="this.value=\'\';this.style.color=\'black\';return false;">\n'
                 '<input type="hidden" name="%(create_wiki_button)s" '
                        'value="1">\n'
-                '<input type="submit" value="Create wiki">\n'
+                '<span class="description">%(after_form)s</span>'
+                '</span>'
+                '<span class="submit"><input type="submit" value="Create wiki"></span>\n'
                 '</form>\n' % d)
     else:
         new_user_link = ('%s%s?new_user=1' %
@@ -35,4 +47,5 @@ def execute(macro, args, formatter=None):
         wiki_create_form = ('You must be logged in to create a wiki!  '
                             'To create an account, go '
                             '<a href="%s">here</a>.' % new_user_link)
-    return formatter.rawHTML(wiki_create_form) 
+    return formatter.rawHTML('<span id="wikiCreateForm">%s</span>' %
+                             wiki_create_form) 
